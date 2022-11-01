c7ebc623a648bdf1293ac9397eda0d6dbcac446a
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-01 12:32:18 +0530

    completed features RD auto debit, deposit account intrest credit

--- main/java/listener/AppListener.java
+++ main/java/listener/AppListener.java
@@ -2,20 +2,39 @@ package listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 
+import runnables.AutoDebitRDRunnable;
+import runnables.DepositIntrestCreditRunnable;
 import util.Factory;
 
 
 public class AppListener implements ServletContextListener {
+	private Thread depositIntrestCreditThread;
+	private Thread autoDebitRDThread;
 	
 	public void contextInitialized(ServletContextEvent sce)  { 
-    	try {    		
+    	try {    		    		
     		Factory.init();
     		System.out.println("factory initialized");
+    		
+    		depositIntrestCreditThread = new Thread(new DepositIntrestCreditRunnable());
+    		depositIntrestCreditThread.start();
+    		System.out.println("\nDeposit intrest credit thread started in context");
+    		
+    		autoDebitRDThread = new Thread(new AutoDebitRDRunnable());
+    		autoDebitRDThread.start();
+    		System.out.println("\nAuto Debit RD thread started in context");
+    		
     	} catch(Exception e) {
     		System.out.println(e.getMessage());
     	}
     }
+	
+	
+	public void contextDestroyed(ServletContextEvent sce) {
+		depositIntrestCreditThread.interrupt();
+		autoDebitRDThread.interrupt();
+		System.out.println("context destroyed");
+	}
 	
 }
