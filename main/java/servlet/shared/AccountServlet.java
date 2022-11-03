1c325167024098e7755372dacc4324804be9810d
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-03 13:16:25 +0530

    updated account servlet as shared

--- main/java/servlet/shared/AccountServlet.java
+++ main/java/servlet/shared/AccountServlet.java
@@ -1,4 +1,4 @@
-package servlet.employee;
+package servlet.shared;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import constant.Role;
 import dao.RegularAccountDAO;
 import model.account.RegularAccount;
 import util.Factory;
@@ -23,7 +24,8 @@ public class AccountServlet extends HttpServlet {
 		RegularAccountDAO accountDAO = Factory.getRegularAccountDAO();
 		RegularAccount account = null;
 		
-		boolean isError = false, exceptionOccured = false;
+		Role role = null;
+		boolean isError = false, exceptionOccured = false, pageFound = true;
 		String path = req.getPathInfo(), action = "", msg = "", queryMsg = null, status = null;
 		String [] result;
 		long accountNo = -1;
@@ -46,13 +48,13 @@ public class AccountServlet extends HttpServlet {
 			path = path.substring(1);
 			result = path.split("/");
 			
+			role = (Role) req.getSession(false).getAttribute("role"); 
 			branchId = (Integer) req.getSession(false).getAttribute("branch-id");
 			
 			accountNo = Long.parseLong(result[0]);
 			action = result[1];
 			
 			account = accountDAO.get(accountNo);
-			
 			if(account == null || account.getBranchId() != branchId) {
 				isError = true;
 				msg = "Account not found !!!";
@@ -61,20 +63,35 @@ public class AccountServlet extends HttpServlet {
 			if(!isError) {
 				req.setAttribute("account", account);
 				
+				pageFound = true;
+				// Generic switch
 				switch(action) {
 					case "view":
 								req.setAttribute("actionType", 1);
 								req.getRequestDispatcher("/jsp/components/viewAccount.jsp").include(req, res); break;
 					case "transaction-history":
 												req.setAttribute("actionType", 0);
+												req.setAttribute("accountCategory", 0);
 												req.getRequestDispatcher("/jsp/components/accountTransactionHistory.jsp").include(req, res); break;
-					case "close":
-								req.setAttribute("accountNo", accountNo);
-								req.getRequestDispatcher("/jsp/employee/closeAccountConfirmation.jsp").include(req, res); break;
-					default: 
-							isError = true;
-							msg = "page not found !!!";
+					default: pageFound = false;
 				}
+				
+				// role specific switch
+				if(role == Role.EMPLOYEE && !pageFound) {
+					pageFound = true;
+					
+					switch(action) {
+						case "close":
+									req.setAttribute("accountNo", accountNo);
+									req.getRequestDispatcher("/jsp/employee/closeAccountConfirmation.jsp").include(req, res); break;
+						default: pageFound = false;
+					}
+				}
+			}
+			
+			if(!pageFound) {
+				isError = true;
+				msg = "page not found !!!";
 			}
 		} catch(ClassCastException e) {
 			System.out.println(e.getMessage());
@@ -92,6 +109,37 @@ public class AccountServlet extends HttpServlet {
 			if(isError || exceptionOccured) {
 				System.out.println(msg);
 				out.println(Util.createNotification(msg, "danger"));
+				req.setAttribute("actionType", 0);
+				req.getRequestDispatcher("/jsp/components/viewAccount.jsp").include(req, res);
+			}
+			
+			out.close();
+		}
+	}
+	
+	
+	// Handles the post request and redirects to the view page.
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {		
+		PrintWriter out = res.getWriter();
+		
+		Role role = null;
+		boolean exceptionOccured = false, isError = false;
+		String errorMsg = "", userType = "", redirectURI = "";
+		long accountNo;
+		
+		try {
+			role = (Role) req.getSession(false).getAttribute("role");
+			userType = Role.getName(role);
+			accountNo = Long.parseLong(req.getParameter("account-no"));
+			redirectURI = String.format("/bank-app/%s/account/%d/view", userType, accountNo);
+			res.sendRedirect(redirectURI);
+		} catch(NumberFormatException e) {
+			exceptionOccured = true;
+			errorMsg = "internal error";
+		} finally {
+			
+			if(isError || exceptionOccured) {
+				out.println(Util.createNotification(errorMsg, "danger"));
 				req.setAttribute("actionType", 0);
 				req.getRequestDispatcher("/jsp/components/viewAccount.jsp").include(req, res);
 			}
