1cd8169a5bf140459c64fd743ebe8237c7e65eae
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-04 11:55:48 +0530

    added method account DAO

--- main/java/dao/AccountDAO.java
+++ main/java/dao/AccountDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+
+import constant.AccountCategory;
 
 public class AccountDAO {
 	public boolean delete(Connection conn, long accountNo) throws SQLException {
@@ -82,5 +86,48 @@ public class AccountDAO {
 			throw new SQLException(msg);
 		else
 			return beforeBalance;
+	}
+	
+	
+	public void closeAccount(Connection conn, long accountNo, AccountCategory category) throws SQLException {
+		PreparedStatement stmt1 = null, stmt2 = null;
+		
+		LocalDate today = LocalDate.now();
+		String msg = "";
+		boolean exceptionOccured = false;
+		
+		try {
+			stmt1 = conn.prepareStatement("UPDATE account SET closing_date = ? WHERE account_no = ?");
+			
+			stmt1.setDate(1, Date.valueOf(today));
+			stmt1.setLong(2, accountNo);
+			stmt1.executeUpdate();
+			
+			// category 0 - regular account, update active status to false.
+			if(category == AccountCategory.REGULAR) {
+				stmt2 = conn.prepareStatement("UPDATE regular_account SET active = false WHERE account_no = ?");
+				stmt2.setLong(1, accountNo);
+				stmt2.executeUpdate();
+			}
+			
+			// update in cache.
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+            exceptionOccured = true;
+            msg = "internal error";
+        } finally {
+            try {
+                if(stmt1 != null)
+                    stmt1.close();
+            } catch(SQLException e) { System.out.println(e.getMessage()); }
+            
+            try {
+                if(stmt2 != null)
+                    stmt2.close();
+            } catch(SQLException e) { System.out.println(e.getMessage()); }
+        }
+				
+		if(exceptionOccured)
+			throw new SQLException(msg);
 	}
 }
