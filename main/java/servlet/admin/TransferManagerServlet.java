fa30f28474f4ea655c58e484e8c5bcab9e365c53
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-11 12:38:25 +0530

    fixed bug updated cache on transfer

--- main/java/servlet/admin/TransferManagerServlet.java
+++ main/java/servlet/admin/TransferManagerServlet.java
@@ -14,19 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.BranchDAO;
 import model.Branch;
+import model.user.Employee;
 import util.Factory;
+import util.Util;
 
 
 public class TransferManagerServlet extends HttpServlet {
-	private BranchDAO branchDAO;
-	
-	
-	public void init() {
-		branchDAO = Factory.getBranchDAO();
-	}
-	
-	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		BranchDAO branchDAO = Factory.getBranchDAO();
 		Collection<Branch> branches = branchDAO.getAll();
 		req.setAttribute("values", branches);
 		req.getRequestDispatcher("/jsp/admin/transferManager.jsp").include(req, res);
@@ -37,12 +32,15 @@ public class TransferManagerServlet extends HttpServlet {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		
+		BranchDAO branchDAO = Factory.getBranchDAO();
 		PrintWriter out = res.getWriter();
+		
+		Employee firstBranchManager = null, secondBranchManager = null;
 		Branch firstBranch = null, secondBranch = null;
 
 		int firstBranchId = -1, secondBranchId = -1;
-		long firstBranchManagerId = -1, secondBranchManagerId = -1;
-		boolean isError = false;
+		boolean isError = false, exceptionOccured = false;
+		String msg = "", status;
 		
 		try {
 			firstBranchId = Integer.parseInt(req.getParameter("first-branch-id"));
@@ -50,49 +48,60 @@ public class TransferManagerServlet extends HttpServlet {
 			
 			// Cannot transfer to same branch.
 			if(firstBranchId == secondBranchId) {
-				out.println("<div class='notification danger'>" + "cannot transfer to same branch" + "</div>");
 				isError = true;
+				msg = "cannot transfer to same branch";
 			}
 			
-		} catch(NumberFormatException e) {			
-			out.println("<div class='notification danger'>" + "invalid input" + "</div>");
+			if(!isError) {
+	            firstBranch = branchDAO.get(firstBranchId);
+	            secondBranch = branchDAO.get(secondBranchId);
+	            
+	            // Handle invalid branch id's
+	            if(firstBranch == null || secondBranch == null) {
+	            	isError = true;
+					msg = "invalid branches !!!";
+	            }
+			}
+			
+			if(!isError) {
+				conn = Factory.getDataSource().getConnection();
+	            stmt = conn.prepareStatement("UPDATE manager SET branch_id = ? WHERE id = ?");
+	            
+	            synchronized (firstBranch) {
+					synchronized (secondBranch) {
+		            	firstBranchManager = firstBranch.getManager();
+		            	secondBranchManager = secondBranch.getManager();
+		            	
+		            	// update manager's branch id's
+		                stmt.setInt(1, secondBranchId);
+		                stmt.setLong(2, firstBranchManager.getId());
+		                stmt.executeUpdate();
+		
+		                stmt.setInt(1, firstBranchId);
+		                stmt.setLong(2, secondBranchManager.getId());
+		                stmt.executeUpdate();
+		                
+		    			// remove managers from cache.
+		                firstBranchManager.setBranchId(secondBranchId);
+		                firstBranchManager.setBranchName(secondBranch.getName());
+		                
+		                secondBranchManager.setBranchId(firstBranchId);
+		                secondBranchManager.setBranchName(firstBranch.getName());
+		                
+		                firstBranch.assignManager(secondBranchManager);
+		                secondBranch.assignManager(firstBranchManager);
+		                
+		                msg = "managers transfer successfull";
+					}
+	            }
+			}
+		}  catch(NumberFormatException e) {			
+			System.out.println(e.getMessage());
 			isError = true;
-		}
-		
-		// Return and render page.
-		if(isError) {
-			doGet(req, res);
-			out.close();
-			return;
-		}
-		
-		try {
-			conn = Factory.getDataSource().getConnection();
-            stmt = conn.prepareStatement("UPDATE manager SET branch_id = ? WHERE id = ?");
-            
-            firstBranch = branchDAO.get(firstBranchId);
-            secondBranch = branchDAO.get(secondBranchId);
-            
-            // Handle invalid branch id's
-            if(firstBranch == null || secondBranch == null) {
-				out.println("<div class='notification danger'>" + "invalid branches !!!" + "</div>");
-            } else {
-            	firstBranchManagerId = firstBranch.getManager().getId();
-            	secondBranchManagerId = secondBranch.getManager().getId();
-            	
-            	// update manager's branch id's
-                stmt.setInt(1, secondBranchId);
-                stmt.setLong(2, firstBranchManagerId);
-                stmt.executeUpdate();
-
-                stmt.setInt(1, firstBranchId);
-                stmt.setLong(2, secondBranchManagerId);
-                stmt.executeUpdate();
-                
-    			out.println("<div class='notification success'>" + "managers transfer successfull" + "</div>");
-            }
+			msg = "invalid input";
 		} catch(SQLException e) {
-			out.println("<div class='notification danger'>" + "managers transfer failed" + "</div>");
+			exceptionOccured = true;
+			msg = e.getMessage();
 		} finally {
 
             try {
@@ -105,6 +114,9 @@ public class TransferManagerServlet extends HttpServlet {
                     conn.close();
             } catch(SQLException e) { System.out.println(e.getMessage()); }
             
+            
+            status = (isError || exceptionOccured) ? "danger" : "success";
+            out.println(Util.createNotification(msg, status));
             doGet(req, res);
             out.close();
         }
