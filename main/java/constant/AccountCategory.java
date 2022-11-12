49281d7df4ca401b4b0190630ec2fb0158c2e9c7
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-12 20:56:54 +0530

    updated, added constants

--- main/java/constant/AccountCategory.java
+++ main/java/constant/AccountCategory.java
@@ -8,4 +8,23 @@ public enum AccountCategory {
 	public final int id;
 	
 	private AccountCategory(int id) { this.id = id; }
+	
+	public static String getName(AccountCategory category) {
+		switch(category) {
+			case REGULAR: return "account";
+			case DEPOSIT: return "deposit";
+			case LOAN: return "loan";
+			default: return "";
+		}
+	}
+	
+	
+	public static AccountCategory getCategory(int type) {
+		switch(type) {
+			case 0: return AccountCategory.REGULAR;
+			case 1: return AccountCategory.DEPOSIT;
+			case 2: return AccountCategory.LOAN;
+			default: return null;
+		}
+	}
 }
